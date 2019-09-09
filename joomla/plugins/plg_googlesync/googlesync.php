9ef914ae0fe36655376c3db3337b672d4b563fa6
Matteo Bonacini <matteo.bonacini@soluzionifutura.it>
2019-09-09 15:00:02 +0200

    WORKA

--- joomla/plugins/plg_googlesync/googlesync.php
+++ joomla/plugins/plg_googlesync/googlesync.php
@@ -3,9 +3,9 @@ defined('_JEXEC') or die;
 
 define('SCOPES', implode(' ', array(
     Google_Service_Directory::ADMIN_DIRECTORY_USER,
-    Google_Service_Directory::ADMIN_DIRECTORY_GROUP,
-    Google_Service_Reports::ADMIN_REPORTS_AUDIT_READONLY,
-    Google_Service_Reports::ADMIN_REPORTS_USAGE_READONLY
+    //Google_Service_Directory::ADMIN_DIRECTORY_GROUP,
+    //Google_Service_Reports::ADMIN_REPORTS_AUDIT_READONLY,
+    //Google_Service_Reports::ADMIN_REPORTS_USAGE_READONLY
   )
 ));
 
@@ -17,16 +17,25 @@ class plgUserGoogleSync extends JPlugin {
   public function __construct(&$subject, $config = array()) {
     parent::__construct($subject, $config);
 
-    $this->client = new Google_Client();
-    $this->client->setSubject("admin@liceoariostospallanzani-re.edu.it");
-    $this->client->setScopes(SCOPES);
-    $this->client->setAuthConfig(JPATH_ROOT.'/plugins/user/googlesync/credentials.json');
+    //$this->client = new Google_Client();
+    //$this->client->setSubject("admin@liceoariostospallanzani-re.edu.it");
+    //$this->client->setScopes(SCOPES);
+    //$this->client->setAuthConfig(JPATH_ROOT.'/plugins/user/googlesync/credentials.json');
+    $this->client = $this->getClient();
     $this->service = new Google_Service_Directory($this->client);
+  }
+
+  private function getClient() {
+    $client = new Google_Client();
+    $client->setSubject("admin@liceoariostospallanzani-re.edu.it");
+    $client->setScopes(SCOPES);
+    $client->setAuthConfig(JPATH_ROOT.'/plugins/user/googlesync/credentials.json');
+    return $client;
   }
 
   function onUserBeforeSave($old_user, $is_new, $new_user) {
    require __DIR__.'/helpers/create-user.php';
-   action($this->service, "test-joomla", "given_name", "family_name", "password");
+   action($this->service, "testsosuser@liceoariostospallanzani-re.edu.it", "given_name", "family_name", "password");
 	}
 
   function onUserBeforeDelete($user) {
--- joomla/plugins/plg_googlesync/helpers/create-user.php
+++ joomla/plugins/plg_googlesync/helpers/create-user.php
@@ -1,14 +1,25 @@
 <?php
 defined('_JEXEC') or die;
 
-function action($service, $email, $givenName, $familyName, $password, $id = null) {
+class Response{
+  public $statusCode = 200;
+  public $errorMessage = null;
+  public $result = null;
+
+  public function getStatusCode()
+  {
+    return $this->statusCode;
+  }
+}
+
+function action($service, $email, $givenName = '', $familyName = '', $password = '', $id = null) {
   $user = new Google_Service_Directory_User();
   if($id != null) {
     $user->setId($id);
   }
 
   $user->setPrimaryEmail($email);
-
+  //die(var_dump($user));
   $username = new Google_Service_Directory_UserName();
   $username->givenName = $givenName;
   $username->familyName = $familyName;
@@ -16,11 +27,13 @@ function action($service, $email, $givenName, $familyName, $password, $id = null
   $user->setName($username);
 
   $user->setPassword($password);
-  try {
-    $service->users->insert($user);
-  } catch (Google_Service_Exception $err) {
-    die($err);
+
+  $response = new Response();
+  try{
+    $response = $service->users->insert($user);
+  } catch (Google_Service_Exception $googleServiceException) {
+    die($googleServiceException);
   }
+  //die(var_dump($response));
   die();
-  //$response = $service->users->getUsers();
 }
